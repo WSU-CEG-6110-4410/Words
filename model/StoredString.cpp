13e620b50dcf579f944106e9b162b6aab8aea5b7
Erik M. Buck <erik.buck@sbcglobal.net>
2022-03-08 09:56:50 -0500

    issue10 insert character at index

--- model/StoredString.cpp
+++ model/StoredString.cpp
@@ -4,19 +4,17 @@
 namespace WSU {
 namespace Model {
 
-    void StoredString::appendCharacter(char c) { m_string += c; }
+void StoredString::appendCharacter(char c) { m_string += c; }
 
-    void StoredString::removeCharacterAtIndex(std::size_t index)
-    {
-        m_string.erase(index, 1);
-    }
+void StoredString::removeCharacterAtIndex(std::size_t index) {
+  m_string.erase(index, 1);
+}
 
-    void StoredString::insertCharacterAtIndex(char c, std::size_t index)
-    {
-        if (index <= m_string.size()) {
-            m_string.insert(index, 1, c);
-        }
-    }
+void StoredString::insertCharacterAtIndex(char c, std::size_t index) {
+  if (index <= m_string.size()) {
+    m_string.insert(index, 1, c);
+  }
+}
 
 } // namespace Model
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -4,50 +4,70 @@
 TEST(Assert, empty) { GTEST_ASSERT_EQ(true, true); }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/7)
-TEST(assert, issue7_createStoredString)
-{
-    ASSERT_NO_FATAL_FAILURE(WSU::Model::StoredString storedString {});
+TEST(StoredString, issue7_createStoredString) {
+  ASSERT_NO_FATAL_FAILURE(WSU::Model::StoredString storedString{});
 }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/7)
-TEST(assert, issue7_zeroContenets)
-{
-    WSU::Model::StoredString storedString {};
-    GTEST_ASSERT_EQ("", storedString.getString());
+TEST(StoredString, issue7_zeroContenets) {
+  WSU::Model::StoredString storedString{};
+  GTEST_ASSERT_EQ("", storedString.getString());
 }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/9)
-TEST(assert, issue9_append)
-{
-    WSU::Model::StoredString storedString {};
-    GTEST_ASSERT_EQ("", storedString.getString());
-    storedString.appendCharacter('W');
-    GTEST_ASSERT_EQ("W", storedString.getString());
-    storedString.appendCharacter('o');
-    GTEST_ASSERT_EQ("Wo", storedString.getString());
+TEST(StoredString, issue9_append) {
+  WSU::Model::StoredString storedString{};
+  GTEST_ASSERT_EQ("", storedString.getString());
+  storedString.appendCharacter('W');
+  GTEST_ASSERT_EQ("W", storedString.getString());
+  storedString.appendCharacter('o');
+  GTEST_ASSERT_EQ("Wo", storedString.getString());
 }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/11)
-TEST(assert, issue11_remove)
-{
-    WSU::Model::StoredString storedString {};
-    storedString.appendCharacter('W');
-    storedString.appendCharacter('o');
-    storedString.appendCharacter('r');
-    storedString.appendCharacter('l');
-    storedString.appendCharacter('d');
-    storedString.appendCharacter('!');
-    GTEST_ASSERT_EQ("World!", storedString.getString());
-    storedString.removeCharacterAtIndex(5); // remove at end
-    GTEST_ASSERT_EQ("World", storedString.getString());
-    storedString.removeCharacterAtIndex(1); // remove in middle
-    GTEST_ASSERT_EQ("Wrld", storedString.getString());
-    storedString.removeCharacterAtIndex(0); // remove at start
-    GTEST_ASSERT_EQ("rld", storedString.getString());
+TEST(StoredString, issue11_remove) {
+  WSU::Model::StoredString storedString{};
+  storedString.appendCharacter('W');
+  storedString.appendCharacter('o');
+  storedString.appendCharacter('r');
+  storedString.appendCharacter('l');
+  storedString.appendCharacter('d');
+  storedString.appendCharacter('!');
+  GTEST_ASSERT_EQ("World!", storedString.getString());
+  storedString.removeCharacterAtIndex(5); // remove at end
+  GTEST_ASSERT_EQ("World", storedString.getString());
+  storedString.removeCharacterAtIndex(1); // remove in middle
+  GTEST_ASSERT_EQ("Wrld", storedString.getString());
+  storedString.removeCharacterAtIndex(0); // remove at start
+  GTEST_ASSERT_EQ("rld", storedString.getString());
 }
 
-int main(int argc, char* argv[])
-{
-    ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+/// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/10)
+TEST(StoredString, issue10_insert) {
+  WSU::Model::StoredString storedString{};
+
+  // Insert into empty string
+  storedString.insertCharacterAtIndex('W', 0);
+  GTEST_ASSERT_EQ("W", storedString.getString());
+
+  storedString.appendCharacter('o');
+  storedString.appendCharacter('r');
+  storedString.appendCharacter('d');
+
+  // Insert into middle of string
+  storedString.insertCharacterAtIndex('l', 3);
+  GTEST_ASSERT_EQ("World", storedString.getString());
+
+  // Insert at end of non-empty string
+  storedString.insertCharacterAtIndex('!', 5);
+  GTEST_ASSERT_EQ("World!", storedString.getString());
+
+  // Insert past end of non-empty string and string is not modified
+  storedString.insertCharacterAtIndex('!', 10);
+  GTEST_ASSERT_EQ("World!", storedString.getString());
+}
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
