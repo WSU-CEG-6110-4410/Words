3a025a715631c77db2d9cc5780eee91a5d655621
Erik M. Buck <erik.buck@sbcglobal.net>
2022-03-10 10:52:30 -0500

    issue15 added using to shorten code

--- tests/commandTests.cpp
+++ tests/commandTests.cpp
@@ -1,32 +1,33 @@
 #include "InsertCharacterAtIndexCommand.h"
 #include <gtest/gtest.h>
 
+using WSU::Model::InsertCharacterAtIndexCommand;
+using WSU::Model::StoredString;
+
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/15)
-TEST(InsertCharacterAtIndexCommand, issue15_create_commnd)
+TEST(InsertCharacterAtIndexCommand, issue15_create_command)
 {
-    WSU::Model::StoredString::p_t string_p { new WSU::Model::StoredString {} };
-    WSU::Model::Command::p_t command_p {
-        new WSU::Model::InsertCharacterAtIndexCommand { string_p, 'H', 0 }
-    };
+    StoredString::p_t string_p { new WSU::Model::StoredString {} };
+    WSU::Model::Command::p_t command_p { new InsertCharacterAtIndexCommand {
+        string_p, 'H', 0 } };
     ASSERT_NE(nullptr, command_p);
 }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/15)
-TEST(InsertCharacterAtIndexCommand, issue15_run_commnd)
+TEST(InsertCharacterAtIndexCommand, issue15_run_command)
 {
-    WSU::Model::StoredString::p_t string_p { new WSU::Model::StoredString {} };
-    WSU::Model::Command::p_t command_p {
-        new WSU::Model::InsertCharacterAtIndexCommand { string_p, 'H', 0 }
-    };
+    StoredString::p_t string_p { new WSU::Model::StoredString {} };
+    WSU::Model::Command::p_t command_p { new InsertCharacterAtIndexCommand {
+        string_p, 'H', 0 } };
     command_p->run();
     GTEST_ASSERT_EQ("H", string_p->getString());
 }
 
 /// [issue](https://github.com/WSU-CEG-6110-4410/Words/issues/15)
-TEST(InsertCharacterAtIndexCommand, issue15_functor)
+TEST(InsertCharacterAtIndexCommand, issue15_functor_command)
 {
-    WSU::Model::StoredString::p_t string_p { new WSU::Model::StoredString {} };
-    WSU::Model::InsertCharacterAtIndexCommand command { string_p, 'H', 0 };
+    StoredString::p_t string_p { new WSU::Model::StoredString {} };
+    InsertCharacterAtIndexCommand command { string_p, 'H', 0 };
     command();
     GTEST_ASSERT_EQ("H", string_p->getString());
 }
